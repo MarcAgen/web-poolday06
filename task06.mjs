77905836dc777cf0b5c60d8487ce58f1680ed623
Marc Agen <marc.agen@epitech.eu>
2026-01-28 14:26:18 +0100

    [FIX]: partialy fix task06

--- task06.mjs
+++ task06.mjs
@@ -6,10 +6,11 @@
 */
 
 export function arrayFiltering(array, test) {
-    const tab = [];
+    var tab = [];
 
     for (let i = 0; i < array.length; i++) {
-        if (test(array[i]) == true)
+        if (test(array[i]))
             tab.push(i);
     }
+    return tab;
 }
--- task06_tester.mjs
+++ task06_tester.mjs
@@ -9,6 +9,6 @@ import { arrayFiltering } from "./task06.mjs";
 
 const toFilter = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const res = arrayFiltering(toFilter, function (value) {
-return value % 3 === 0;
+    return value % 3 === 0;
 });
 console.log(res);
